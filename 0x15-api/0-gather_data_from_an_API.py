df73f3bb6b76eae34e8d0d8bb8aa4a652e1fcce1
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-03 01:55:38 +0000

    Always exit status 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,36 +11,39 @@ user_endpoint = "https://jsonplaceholder.typicode.com/users/"
 def get_todos_by_userid(user_id):
     """Get TODO list for a user identified by `user_id`"""
     todos = requests.get(user_endpoint + user_id + '/todos')
-    if todos.status_code == 200:
+    try:
+        todos.raise_for_status()
         return todos.json()
-    else:
-        exit(1)
+    except:
+        exit
 
 
-def get_name_by_userid(user_id):
+def get_user_by_userid(user_id):
     """Get the full name of auser identified by `user_id`"""
     user = requests.get(user_endpoint + user_id)
-    if user.status_code == 200:
+    try:
+        user.raise_for_status()
         return user.json()
-    else:
-        exit(1)
+    except:
+        exit
 
 
 def format_user_todos(user_id):
     """Format employee TODO list as a string"""
     todos = get_todos_by_userid(user_id)
-    complete = list(filter(lambda t: t.get('completed') is True, todos))
-    name = get_name_by_userid(user_id).get('name')
-    output = "Employee {} is done with tasks({}/{}):\n\t".format(name,
-                                                                 len(complete),
-                                                                 len(todos))
-    output += '\n\t'.join(map(lambda c: c.get('title', ''), complete))
-    return output
+    completed = list(filter(lambda t: t.get('completed') is True, todos))
+    name = get_user_by_userid(user_id).get('name')
+    output = "Employee {} is done with tasks({}/{}):".format(name,
+                                                             len(completed),
+                                                             len(todos))
+    titles = '\n\t'.join(map(lambda c: c.get('title', ''), completed))
+    if not titles:
+        return output
+    return output + '\n\t' + titles
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
     user_id = sys.argv[1]
     print(format_user_todos(user_id))
