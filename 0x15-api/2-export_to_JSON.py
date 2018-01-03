7ea6cecdb1a7b87ec41aab0ba8c5eca605264384
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-03 02:18:16 +0000

    Alphabetize imports

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script to get information from the TODO api endpoint as it
 pertains to a particular employee identified by ID."""
-import sys
 import json
 import requests
+import sys
 
 
 todo_endpoint = "https://jsonplaceholder.typicode.com/todos"
