e690751a063c887b3008d4aa3b01f91da486a821
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-23 17:22:14 +0000

    exec only if file is present

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 # Issue is a misspelled resource in `wp-settings.php`.
 exec {'sed phpp':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/bin',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
 }
