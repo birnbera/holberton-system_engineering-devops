f97609a2d0538ee08e1359c89f9b2e4f53e502ba
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-28 08:26:52 +0000

    Put single quotes on outside of sed commands to avoid escape warnings from puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Set ulimit on nginx max file descriptors to system soft limit
 exec {'ulimit':
-  command => "sed -i 's/\(ULIMIT=\"-n \)15/\18000/' /etc/default/nginx",
+  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => 'test -e /etc/default/nginx',
 }
